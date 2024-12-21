66fb55928c224ba9145047d61c82c71f49bbb511
Aldo Cortesi <aldo@corte.si>
2024-12-21 19:06:38 +1300

    Use #[skip_serializing_none] rather than individual annotations

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -174,18 +174,17 @@ pub enum SubjectReferenceType {
     SubjectTypeProduct,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct VideoMetadata {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub end_offset: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub start_offset: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct CodeExecutionResult {
     pub outcome: Outcome,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub output: Option<String>,
 }
 
@@ -201,18 +200,17 @@ pub struct FileData {
     pub mime_type: String,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct FunctionCall {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub args: Option<serde_json::Value>,
     pub name: String,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct FunctionResponse {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
     pub name: String,
     pub response: serde_json::Value,
@@ -224,115 +222,83 @@ pub struct Blob {
     pub mime_type: String,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Part {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub video_metadata: Option<VideoMetadata>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub code_execution_result: Option<CodeExecutionResult>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub executable_code: Option<ExecutableCode>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub file_data: Option<FileData>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub function_call: Option<FunctionCall>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub function_response: Option<FunctionResponse>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub inline_data: Option<Blob>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub text: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Content {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub parts: Option<Vec<Part>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub role: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Schema {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub min_items: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub example: Option<serde_json::Value>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub property_ordering: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub pattern: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub minimum: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub default: Option<serde_json::Value>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub any_of: Option<Vec<Schema>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub max_length: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub title: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub min_length: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub min_properties: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub max_items: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub maximum: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub nullable: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub max_properties: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub r#type: Option<Type>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub r#enum: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub format: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub items: Option<Box<Schema>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub properties: Option<std::collections::HashMap<String, Schema>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub required: Option<Vec<String>>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct SafetySetting {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub method: Option<HarmBlockMethod>,
     pub category: HarmCategory,
     pub threshold: HarmBlockThreshold,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct FunctionDeclaration {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub response: Option<Schema>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
     pub name: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub parameters: Option<Schema>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GoogleSearch {}
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct DynamicRetrievalConfig {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub mode: Option<DynamicRetrievalConfigMode>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub dynamic_threshold: Option<f64>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GoogleSearchRetrieval {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub dynamic_retrieval_config: Option<DynamicRetrievalConfig>,
 }
 
@@ -341,173 +307,134 @@ pub struct VertexAISearch {
     pub datastore: String,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct VertexRAGStoreRAGResource {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub rag_corpus: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub rag_file_ids: Option<Vec<String>>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct VertexRAGStore {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub rag_corpora: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub rag_resources: Option<Vec<VertexRAGStoreRAGResource>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub similarity_top_k: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub vector_distance_threshold: Option<f64>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Retrieval {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub vertex_ai_search: Option<VertexAISearch>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub vertex_rag_store: Option<VertexRAGStore>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct ToolCodeExecution {}
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Tool {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub function_declarations: Option<Vec<FunctionDeclaration>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub retrieval: Option<Retrieval>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub google_search: Option<GoogleSearch>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub google_search_retrieval: Option<GoogleSearchRetrieval>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub code_execution: Option<ToolCodeExecution>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct FunctionCallingConfig {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub mode: Option<FunctionCallingConfigMode>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub allowed_function_names: Option<Vec<String>>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct ToolConfig {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub function_calling_config: Option<FunctionCallingConfig>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct PrebuiltVoiceConfig {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub voice_name: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct VoiceConfig {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub prebuilt_voice_config: Option<PrebuiltVoiceConfig>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct SpeechConfig {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub voice_config: Option<VoiceConfig>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GenerationConfigRoutingConfigAutoRoutingMode {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub model_routing_preference: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GenerationConfigRoutingConfigManualRoutingMode {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub model_name: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GenerationConfigRoutingConfig {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub auto_mode: Option<GenerationConfigRoutingConfigAutoRoutingMode>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub manual_mode: Option<GenerationConfigRoutingConfigManualRoutingMode>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GenerateContentConfig {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub system_instruction: Option<Content>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub temperature: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub top_p: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub top_k: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub candidate_count: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub max_output_tokens: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub stop_sequences: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub response_logprobs: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub logprobs: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub presence_penalty: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub frequency_penalty: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub seed: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub response_mime_type: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub response_schema: Option<Schema>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub routing_config: Option<GenerationConfigRoutingConfig>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub safety_settings: Option<Vec<SafetySetting>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub tools: Option<Vec<Tool>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub tool_config: Option<ToolConfig>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub cached_content: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub response_modalities: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub media_resolution: Option<MediaResolution>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub speech_config: Option<SpeechConfig>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GenerateContentParameters {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub model: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub contents: Option<Vec<Content>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub config: Option<GenerateContentConfig>,
 }
 
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Citation {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub end_index: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub license: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub publication_date: Option<Date>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub start_index: Option<i64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub title: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub uri: Option<String>,
 }
